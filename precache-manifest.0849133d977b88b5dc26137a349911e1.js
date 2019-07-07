ffa9f9bbcd36203e0f59201c71405ecb80e31a64
nghiadangtrong <nghiadangtrongn92@gmail.com>
2019-07-07 11:50:57 +0700

    Updates

---
+++ precache-manifest.0849133d977b88b5dc26137a349911e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b470c2f328ce99103d170274d8035ced",
+    "url": "/testing/index.html"
+  },
+  {
+    "revision": "9a94ae0d2a258f88fb9b",
+    "url": "/testing/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "751e5179ea80c0ddb0f0",
+    "url": "/testing/static/css/main.86f87eb8.chunk.css"
+  },
+  {
+    "revision": "9a94ae0d2a258f88fb9b",
+    "url": "/testing/static/js/2.c40d8762.chunk.js"
+  },
+  {
+    "revision": "751e5179ea80c0ddb0f0",
+    "url": "/testing/static/js/main.aebc9f50.chunk.js"
+  },
+  {
+    "revision": "f1f30a4328be139affc6",
+    "url": "/testing/static/js/runtime~main.dfd23d7b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/testing/static/media/SEGA.12a3db8b.woff"
+  }
+]);
